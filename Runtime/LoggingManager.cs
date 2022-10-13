9944bbb978a80f407425276da3d1afd256671a1b
sus-taguchi-t <taguchi-t@m.sus-g.co.jp>
2022-10-13 15:44:53 +0900

    Consolidate process that sets LogLevel to LogOutputChecker and use dictionary to cache Logger

--- Runtime/LoggingManager.cs
+++ Runtime/LoggingManager.cs
@@ -1,31 +1,42 @@
-﻿namespace Extreal.Core.Logging
+﻿using System.Collections.Generic;
+namespace Extreal.Core.Logging
 {
     public static class LoggingManager
     {
         private static ILogWriter s_writer = new UnityDebugLogWriter();
         private static ILogOutputChecker s_checker = new LogLevelLogOutputChecker();
         private static LogLevel s_logLevel = LogLevel.INFO;
+        private static Dictionary<string, Logger> s_loggerDict = new Dictionary<string, Logger>();
 
         public static Logger GetLogger(string logCategory)
         {
-            return new Logger(logCategory, s_writer, s_checker);
+            if (s_loggerDict.ContainsKey(logCategory))
+            {
+                return s_loggerDict[logCategory];
+            }
+            return s_loggerDict[logCategory] = new Logger(logCategory, s_writer, s_checker);
         }
 
         public static void SetLogLevel(LogLevel logLevel)
         {
             s_logLevel = logLevel;
-            s_checker.SetLogLevel(logLevel);
+            SetLogLevel2LogOutputChecker();
         }
 
         public static void SetLogOutputChecker(ILogOutputChecker logOutputChecker)
         {
             s_checker = logOutputChecker;
-            s_checker.SetLogLevel(s_logLevel);
+            SetLogLevel2LogOutputChecker();
         }
 
         public static void SetLogWriter(ILogWriter logWriter)
         {
             s_writer = logWriter;
+        }
+
+        private static void SetLogLevel2LogOutputChecker()
+        {
+            s_checker.SetLogLevel(s_logLevel);
         }
     }
 }
