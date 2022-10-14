082d37755ab1a62c044f9b9a39b34c1be2fc4c9b
sus-taguchi-t <taguchi-t@m.sus-g.co.jp>
2022-10-14 12:28:37 +0900

    Change way to log

--- Runtime/UnityDebugLogWriter.cs
+++ Runtime/UnityDebugLogWriter.cs
@@ -13,42 +13,59 @@ namespace Extreal.Core.Logging
 
         public void LogDebug(string logCategory, string message)
         {
-            Debug.Log($"[{LogLevel.Debug}:{logCategory}] {message}");
+            Debug.Log(LogFormat(logCategory, LogLevel.Debug, message));
         }
 
         public void LogDebug(string logCategory, string message, Exception exception)
         {
-            Debug.Log($"[{LogLevel.Debug}:{logCategory}] {message}\n----------\n{exception}");
+            Debug.Log(LogFormat(logCategory, LogLevel.Debug, message, exception));
         }
 
         public void LogInfo(string logCategory, string message)
         {
-            Debug.Log($"[{LogLevel.Info}:{logCategory}] {message}");
+            Debug.Log(LogFormat(logCategory, LogLevel.Info, message));
         }
 
         public void LogInfo(string logCategory, string message, Exception exception)
         {
-            Debug.Log($"[{LogLevel.Info}:{logCategory}] {message}\n----------\n{exception}");
+            Debug.Log(LogFormat(logCategory, LogLevel.Info, message, exception));
         }
 
         public void LogWarn(string logCategory, string message)
         {
-            Debug.LogWarning($"[{LogLevel.Warn}:{logCategory}] {message}");
+            Debug.LogWarning(LogFormat(logCategory, LogLevel.Warn, message));
         }
 
         public void LogWarn(string logCategory, string message, Exception exception)
         {
-            Debug.LogWarning($"[{LogLevel.Warn}:{logCategory}] {message}\n----------\n{exception}");
+            Debug.LogWarning(LogFormat(logCategory, LogLevel.Warn, message, exception));
         }
 
         public void LogError(string logCategory, string message)
         {
-            Debug.LogError($"[{LogLevel.Error}:{logCategory}] {message}");
+            Debug.LogError(LogFormat(logCategory, LogLevel.Error, message));
         }
 
         public void LogError(string logCategory, string message, Exception exception)
         {
-            Debug.LogError($"[{LogLevel.Error}:{logCategory}] {message}\n----------\n{exception}");
+            Debug.LogError(LogFormat(logCategory, LogLevel.Error, message, exception));
+        }
+
+        private string LogFormat(string logCategory, LogLevel logLevel, string message, Exception exception = null)
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder
+                .Append("[")
+                .Append(logLevel)
+                .Append(":")
+                .Append(logCategory)
+                .Append("] ")
+                .Append(message);
+            if (exception != null)
+            {
+                stringBuilder.Append("\n----------\n").Append(exception);
+            }
+            return stringBuilder.ToString();
         }
     }
 }
